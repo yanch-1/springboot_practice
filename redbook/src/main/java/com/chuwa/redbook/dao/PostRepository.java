78e49fc6d317b39443c954593fa6ec797bd8d8e1
yanch-1 <Cyan9@depaul.edu>
2022-11-02 02:25:50 -0500

    jdbc added

--- redbook/src/main/java/com/chuwa/redbook/dao/PostRepository.java
+++ redbook/src/main/java/com/chuwa/redbook/dao/PostRepository.java
@@ -1,9 +1,77 @@
 package com.chuwa.redbook.dao;
 
 import com.chuwa.redbook.entity.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public class PostRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public PostRepository() {};
+
+    public List<Post> findAll(){
+        String sql = "SELECT * FROM posts";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Post.class));
+    }
+
+    public Optional<Post> findById(long id){
+        String sql = "SELECT * FROM POSTS WHERE ID = ?";
+        Post p = jdbcTemplate.queryForObject(sql,
+                new BeanPropertyRowMapper<Post>(Post.class),
+                new Object[] {id});
+        return Optional.ofNullable(p);
+    }
+
+    public Post save(Post post){
+        final String sql = "INSERT INTO POSTS (" +
+                "title, description, content, create_date_time, update_date_time) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        final KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        int result = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getContent());
+            ps.setObject(4, LocalDateTime.now());
+            ps.setObject(5, LocalDateTime.now());
+            return ps;
+        }, keyHolder);
+
+        if (result > 0) return findById(keyHolder.getKey().longValue()).get();
+        return null;
+    }
+
+    public Post update(Post post){
+        final String sql = "UPDATE POSTS " +
+                "SET title = ?, description = ?, content = ? , update_date_time = ? " +
+                "WHERE ID = ?";
+        jdbcTemplate.update(sql, new Object[] {
+                post.getTitle(),
+                post.getDescription(),
+                post.getContent(),
+                LocalDateTime.now(),
+                post.getId()
+        });
+
+        return post;
+    }
+
+    public void delete(Post post){
+        String sql="DELETE FROM POSTS WHERE ID = ?";
+        jdbcTemplate.update(sql, new Object[] {post.getId()});
+    }
 }
--- redbook/src/main/java/com/chuwa/redbook/entity/Post.java
+++ redbook/src/main/java/com/chuwa/redbook/entity/Post.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(
-        name="post",
+        name="posts",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
 )
 
