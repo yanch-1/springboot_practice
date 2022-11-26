744533789cdec9b9dc883eb1e1ec6628981555cb
yanch-1 <78085774+yanch-1@users.noreply.github.com>
2022-11-26 01:42:25 -0600

    Update RoleRepository.java

--- redbook/src/main/java/com/chuwa/redbook/dao/security/RoleRepository.java
+++ redbook/src/main/java/com/chuwa/redbook/dao/security/RoleRepository.java
@@ -1,11 +1,10 @@
 package com.chuwa.redbook.dao.security;
 
 import com.chuwa.redbook.entity.security.Role;
-import com.chuwa.redbook.entity.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<User, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
